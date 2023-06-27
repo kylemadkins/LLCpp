1cd560b73728f383f790139bbeffa055ced1f2bb
Kyle Adkins <kyle.m.adkins@gmail.com>
2023-06-26 23:26:57 -0400

    Type casting example

--- LLCpp/LLCpp.cpp
+++ LLCpp/LLCpp.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <array>
 
 #include "LLCpp.h"
 
@@ -8,5 +9,13 @@ int main()
 	std::cout << "Please enter your first name:" << '\n';
 	std::cin >> name;
 	std::cout << "Hello, " << name << '\n';
+
+	std::array grades = { 95, 87, 63, 92, 99 };
+	float sum = 0.0f;
+	for (auto grade : grades) {
+		sum += grade;
+	}
+	std::cout << sum / grades.size() << '\n';
+
 	return 0;
 }
